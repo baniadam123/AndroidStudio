fb401ece03a07300bd01b944b90adffa180b0039
baniadam123 <banidamreal@gmail.com>
2022-04-06 08:58:36 +0700

    tab android bani adam

---
+++ app/src/main/java/com/example/tabelactivitybani/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.example.tabelactivitybani.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.tabelactivitybani.R;
+import com.example.tabelactivitybani.ui.Panggilan.PanggilanFragment;
+import com.example.tabelactivitybani.ui.chat.ChatFragment;
+import com.example.tabelactivitybani.ui.status.StatusFragment;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2, R.string.tab_text_3};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position==0){
+            return ChatFragment.newInstance("","");
+        }else if(position==1){
+            return StatusFragment.newInstance("","");
+        }else{
+            return PanggilanFragment.newInstance("","");
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return TAB_TITLES.length;
+    }
+}
